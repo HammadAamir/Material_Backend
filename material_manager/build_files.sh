c2cb0fd0e25766531904aaf51012a998d92bf135
HammadAamir <hamaamir2001@gmail.com>
2024-12-28 19:15:04 +0100

    cors true disabled

--- material_manager/build_files.sh
+++ material_manager/build_files.sh
@@ -1,5 +1,7 @@
 echo "BUILD START"
+python3.9 -m venv env 
+source env/bin/activate
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-
 echo "BUILD END"
