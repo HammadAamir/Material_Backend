a13e5043fb50325626c8a68d2a6c26517c50e098
HammadAamir <hamaamir2001@gmail.com>
2024-12-28 15:23:50 +0100

    env changes

--- material_manager/build_files.sh
+++ material_manager/build_files.sh
@@ -1,7 +1,5 @@
 echo "BUILD START"
-python3.9 -m venv env
-source env/bin/activate
-pip install --upgrade pip
+
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
