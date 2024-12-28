ee27ac963c88ef0960e147d4faa5b9f328a8d2b2
HammadAamir <hamaamir2001@gmail.com>
2024-12-28 19:20:36 +0100

    changesi n reuiqrmeents. txt sfor django

--- material_manager/build_files.sh
+++ material_manager/build_files.sh
@@ -1,7 +1,7 @@
 echo "BUILD START"
-python3.10 -m venv env 
+python3.9 -m venv env 
 source env/bin/activate
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
